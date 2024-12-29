8796b66ed79c8d77f96850edb9bd521b6655a1fa
ErrQD0303 <quocdat3396@gmail.com>
2024-12-30 03:05:19 +0700

    Fix frontend and change session to Redis

--- ReactFrontEnd/src/pages/homeLoader.ts
+++ ReactFrontEnd/src/pages/homeLoader.ts
@@ -1,9 +1,9 @@
 import { LoaderFunctionArgs } from "react-router-dom";
 import authenticateService from "../services/authenticateService";
 
-const loader = async ({ request }: LoaderFunctionArgs) => {
+const loader = async ({ context, params, request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
-  console.log(url.href, request.url);
+  console.log(url.href, context, params, request);
 
   const accessToken = url.href.includes("logout")
     ? null
