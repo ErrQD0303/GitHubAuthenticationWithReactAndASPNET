29dff7d10c668c8901e57dd1e0cd14e3d59616bd
ErrQD0303 <quocdat3396@gmail.com>
2024-12-30 03:15:00 +0700

    Fix frontend and change session to Redis

--- ReactFrontEnd/src/pages/LogOut.tsx
+++ ReactFrontEnd/src/pages/LogOut.tsx
@@ -8,7 +8,7 @@ function LogOut() {
   const [, , removeCookie] = useCookies(["access_token"]);
   useEffect(() => {
     removeCookie("access_token");
-    navigate("/", { state: { from: "logout" } });
+    navigate("/?from=logout");
   }, [removeCookie, navigate]);
 
   return <div>LogOut</div>;
