8af09e94322909261cf35744c7d846e3add8556d
ErrQD0303 <quocdat3396@gmail.com>
2024-12-30 03:01:32 +0700

    Fix frontend and change session to Redis

--- ReactFrontEnd/src/pages/homeLoader.ts
+++ ReactFrontEnd/src/pages/homeLoader.ts
@@ -3,6 +3,7 @@ import cookieStorageService from "../services/cookieStorageService";
 
 const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
+  console.log(url.href, request.url);
 
   const accessToken = url.href.includes("logout")
     ? null
