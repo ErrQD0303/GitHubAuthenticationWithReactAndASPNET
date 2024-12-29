d91c40b47e67fcf2eff73d83511fe54a44a6d0e8
ErrQD0303 <quocdat3396@gmail.com>
2024-12-30 02:19:40 +0700

    Fix frontend and change session to Redis

--- ReactFrontEnd/src/layouts/AppLayout.tsx
+++ ReactFrontEnd/src/layouts/AppLayout.tsx
@@ -1,8 +1,15 @@
 import { Outlet, useLoaderData } from "react-router-dom";
 import { IAppLoader } from "../types/app";
+import { useCookies } from "react-cookie";
 
 function AppLayout() {
   const appLoader = useLoaderData() as IAppLoader;
+
+  const [, setCookie] = useCookies(["accessToken"]);
+  if (appLoader.accessToken) {
+    console.log("Setting access token" + appLoader.accessToken);
+    setCookie("accessToken", appLoader.accessToken);
+  }
 
   return (
     <div>
